d520b9c16e102b4dcf2614f7964fa334457dcb3d
antoniopalomequeprogrammer <palomequework@gmail.com>
2022-04-15 09:51:49 +0200

    Crud Viviendas

--- app/Http/Controllers/API/ViviendaController.php
+++ app/Http/Controllers/API/ViviendaController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\API;
+
 use App\Models\Vivienda;
 use App\Models\Inmobiliaria;
 use Illuminate\Http\Request;
@@ -10,6 +11,7 @@ use App\Http\Controllers\API\ResponseController as ResponseController;
 use App\Http\Resources\ViviendaCollection;
 use App\Models\Imagen;
 use App\Services\FileService;
+
 class ViviendaController extends ResponseController
 {
 
@@ -20,65 +22,65 @@ class ViviendaController extends ResponseController
 
 
 
-        $viviendas = Vivienda::where(function ($query) use ($filtros,$request){
+        $viviendas = Vivienda::where(function ($query) use ($filtros, $request) {
 
-            if($request->has('search')){
-                $query->where('titulo','LIKE','%'.$request->search.'%');
+            if ($request->has('search')) {
+                $query->where('titulo', 'LIKE', '%' . $request->search . '%');
             }
         })
-        ->when($filtros['ascensor'] !='', function ($query) use ($filtros){
-            $query->where('ascensor',(int)$filtros['ascensor']);
-        })
-        ->when($filtros['terraza'] !='', function ($query) use ($filtros) {
-            $query->where('terraza', $filtros['terraza']);
-        })
-        ->when($filtros['garaje'] !='', function ($query) use ($filtros) {
-            $query->where('garaje', $filtros['garaje']);
-        })
+            ->when($filtros['ascensor'] != '', function ($query) use ($filtros) {
+                $query->where('ascensor', (int)$filtros['ascensor']);
+            })
+            ->when($filtros['terraza'] != '', function ($query) use ($filtros) {
+                $query->where('terraza', $filtros['terraza']);
+            })
+            ->when($filtros['garaje'] != '', function ($query) use ($filtros) {
+                $query->where('garaje', $filtros['garaje']);
+            })
 
-        ->when($filtros['habitaciones'] != '', function ($query) use ($filtros) {
+            ->when($filtros['habitaciones'] != '', function ($query) use ($filtros) {
 
-            if($filtros['habitaciones']>=3){
-                $query->where('habitacion','<',$filtros['habitaciones']);
-            }else{
-                $query->where('habitacion',$filtros['habitaciones']);
-            }
-        })
-        ->when($filtros['banos'] != '', function ($query) use ($filtros) {
-            if($filtros['banos']>=3){
-                $query->where('banos','<',$filtros['banos']);
-            }else{
-                $query->where('banos',$filtros['banos']);
-            }
-        })
-        ->paginate($perPage);
+                if ($filtros['habitaciones'] >= 3) {
+                    $query->where('habitacion', '<', $filtros['habitaciones']);
+                } else {
+                    $query->where('habitacion', $filtros['habitaciones']);
+                }
+            })
+            ->when($filtros['banos'] != '', function ($query) use ($filtros) {
+                if ($filtros['banos'] >= 3) {
+                    $query->where('banos', '<', $filtros['banos']);
+                } else {
+                    $query->where('banos', $filtros['banos']);
+                }
+            })
+            ->paginate($perPage);
 
         $viviendas = new ViviendaCollection($viviendas);
 
         return response()->json($viviendas);
     }
 
-    public function misViviendas(Request $request){
+    public function misViviendas(Request $request)
+    {
         $perPage = $request->get('perPageData');
 
-      $viviendas = Vivienda::where(function ($query) use ($request){
+        $viviendas = Vivienda::where(function ($query) use ($request) {
 
-          if($request->has('search')){
-              $query->where('titulo','LIKE','%'.$request->search.'%');
-          }
+            if ($request->has('search')) {
+                $query->where('titulo', 'LIKE', '%' . $request->search . '%');
+            }
+        })->paginate($perPage);
 
-      })->paginate($perPage);
-
-      return new ViviendaCollection($viviendas);
+        return new ViviendaCollection($viviendas);
     }
 
-    public function vivienda($id){
-        $vivienda = Vivienda::where('id',$id)->with('inmobiliaria')->first();
+    public function vivienda($id)
+    {
+        $vivienda = Vivienda::where('id', $id)->with('inmobiliaria')->first();
 
         $vivienda = new ViviendaResource($vivienda);
 
         return response()->json($vivienda);
-
     }
 
     public function create()
@@ -86,123 +88,18 @@ class ViviendaController extends ResponseController
         //
     }
 
-    // public function store(Request $request)
-    // {
-
-    //     $etiquetas = json_decode($request->etiquetas, true);
-    //     $precioFinal = $request->get('precio_unidad') - ($request->get('precio_unidad') * $request->get('descuento') / 100);
-    //     $precioFinal = $precioFinal + ($precioFinal * $request->get('iva') / 100);
-    //     $precioFinal = round($precioFinal, 2);
-
-
-    //     $establecimiento = Establecimiento::where('user_id', '=', Auth::user()->id)->first();
-
-    //     if (isset($establecimiento)) {
-
-    //         $dataProducto = [
-    //             'categoria_id' => $request->get('categoria_id'),
-    //             'nombre' => $request->get('nombre'),
-    //             'descripcion' => $request->get('descripcion'),
-    //             'precio_unidad' => round($request->get('precio_unidad'), 2),
-    //             'precio_final' => $precioFinal,
-    //             'stock' => $request->get('stock'),
-    //             'iva' => $request->get('iva'),
-    //             'stock' => $request->get('stock'),
-    //             'activo' => $request->get('activo'),
-    //             'visibilidad' => $establecimiento->visibilidad == 1 ? "NO" : "SI",
-    //             'establecimiento_id' => $establecimiento->id,
-
-    //         ];
-
-    //         $producto = Producto::create($dataProducto);
-
-    //         if ($request->imagenes) {
-
-    //             $imagenes = json_decode($request->imagenes, true);
-    //             foreach ($imagenes as $key => $imagen) {
-    //                 $imagen = FileService::guardarArchivo($imagen, "/producto/{$producto->id}", true);
-
-    //                 // $imagen = ImageOptimizer::optimize($imagen);
-
-    //                 Imagenes::create([
-    //                     'url' => $imagen,
-    //                     'producto_id' => $producto->id
-    //                 ]);
-    //             }
-    //         }
-
-    //         // Etiquetas.
-    //         if (isset($etiquetas)) {
-    //             foreach ($etiquetas as $key => $etiqueta) {
-
-
-    //                 $etiquetaEstablecimientoProducto = EtiquetaEstablecimientoProducto::create([
-    //                     'nombre_etiqueta' => $etiqueta['label'],
-    //                     'etiqueta_id' => $etiqueta['id'],
-    //                     'producto_id' => $producto->id,
-    //                 ]);
-
-
-    //                 foreach ($etiqueta['atributos'] as $key => $atributo) {
-    //                     $precio = 0;
-    //                     $activo = 0;
-
-    //                     if (isset($atributo['precio'])) {
-    //                         $precio = $atributo['precio'];
-    //                     }
-
-    //                     if (isset($atributo['activo'])) {
-    //                         $activo = $atributo['activo'];
-    //                     }
-
-    //                     RespuestaEtiqueta::create([
-    //                         'atributo_id' => $atributo['id'],
-    //                         'etiqueta_establecimiento_producto_id' => $etiquetaEstablecimientoProducto->id,
-    //                         'label' => $atributo['label'],
-    //                         'activo' => $activo,
-    //                         'precio' => $precio,
-    //                         // 'etiqueta_id' => $atributo['etiqueta_id'],
-
-    //                     ]);
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     // Busco si existe alguna relación.
-    //     $existeCategoriaEstablecimiento = DB::table('categoria_establecimiento')
-    //         ->where('categoria_id', $request->get('categoria_id'))
-    //         ->where('establecimiento_id', $establecimiento->id)->get();
-
-
-    //     if (count($existeCategoriaEstablecimiento) == 0) {
-
-    //         // Asignar a ese establecimiento la categoria.
-    //         \DB::table('categoria_establecimiento')->insert(
-    //             ['categoria_id' =>  $request->get('categoria_id'), 'establecimiento_id' => $establecimiento->id,]
-    //         );
-    //     }
-
-
-
-    //     return response()->json("ok");
-    // }
-
-
-
-
     public function store(Request $request)
     {
         // Creo los datos de la vivienda y luego las fotos.
         $userId = Auth::user()->id;
-        $inmobiliaria = Inmobiliaria::where('user_id',$userId)->first();
+        $inmobiliaria = Inmobiliaria::where('user_id', $userId)->first();
 
         $viviendaData = [];
         $viviendaData['titulo'] = $request->get('titulo');
         $viviendaData['descripcion'] = $request->get('descripcion');
         $viviendaData['planta'] = $request->get('planta');
         $viviendaData['precio'] = $request->get('precio');
-        $viviendaData['habitacion'] = $request->get('habitaciones');
+        $viviendaData['habitacion'] = $request->get('habitacion');
         $viviendaData['banos'] = $request->get('banos');
         $viviendaData['garaje'] = $request->get('garaje');
         $viviendaData['ascensor'] = $request->get('ascensor');
@@ -217,18 +114,18 @@ class ViviendaController extends ResponseController
 
         if ($request->imagenes) {
 
-           $imagenes = json_decode($request->imagenes, true);
+            $imagenes = json_decode($request->imagenes, true);
             foreach ($imagenes as $key => $imagen) {
-                $imagen = FileService::guardarArchivo($imagen, "/vivivenda/{$vivienda->id}", true);
+                $imagen = FileService::guardarArchivo($imagen, "/vivienda/{$vivienda->id}", true);
 
-                     // $imagen = ImageOptimizer::optimize($imagen);
+                // $imagen = ImageOptimizer::optimize($imagen);
 
                 Imagen::create([
                     'ruta' => $imagen,
                     'vivienda_id' => $vivienda->id
                 ]);
-                 }
-             }
+            }
+        }
 
         return $vivienda;
     }
@@ -245,13 +142,52 @@ class ViviendaController extends ResponseController
         //
     }
 
-    public function update(Request $request, Vivienda $vivienda)
+    public function update(Request $request, $id)
     {
-        //
+        $inputs = $request->get('vivienda');
+
+        // try {
+        Vivienda::where('id', $id)->update([
+            'titulo' => $inputs['titulo'],
+            'descripcion' => $inputs['descripcion'],
+            'precio' => $inputs['precio'],
+            'habitacion' => $inputs['habitacion'],
+            'planta' => $inputs['planta'],
+            'banos' => $inputs['banos'],
+            'ascensor' => $inputs['ascensor'] == "SI" ? 1 : 0,
+            'garaje' => $inputs['garaje'] == "SI" ? 1 : 0,
+            'terraza' => $inputs['terraza'] == "SI" ? 1 : 0,
+            'm2' => $inputs['m2'],
+
+
+        ]);
+
+        if (isset($inputs['estado'])) {
+            Vivienda::where('id', $id)->update([
+                'estado_id' => $inputs['estado'],
+            ]);
+        }
+
+        if (isset($inputs['tipo'])) {
+            Vivienda::where('id', $id)->update([
+                'tipo_id' => $inputs['tipo'],
+            ]);
+        }
+
+
+
+        return response()->json("Vivienda Editada correctamente");
     }
 
-    public function destroy(Vivienda $vivienda)
+
+    public function eliminarVivienda($id)
     {
-        //
+
+        try {
+            Vivienda::where('id', $id)->delete();
+            return "ok";
+        } catch (\Exeptions $e) {
+            return $this->sendError("No se puede borrar la vivienda");
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,78 +19,76 @@ ES FRONT NO CLIENTE EL SCOPE DEL FRONT
 
 
 
-Route::post('estados/index','API\EstadoController@index');
-Route::get('tipos/index','API\TipoController@index');
-Route::get('grupos/index','API\GrupoController@index');
+Route::post('estados/index', 'API\EstadoController@index');
+Route::get('tipos/index', 'API\TipoController@index');
+Route::get('grupos/index', 'API\GrupoController@index');
 Route::post('viviendas/index/{page?}', 'API\ViviendaController@index');
-Route::get('vivienda/{id?}','API\ViviendaController@vivienda');
-Route::get('index/inmobiliarias','API\InmobiliariaController@index');
+Route::get('vivienda/{id?}', 'API\ViviendaController@vivienda');
+Route::get('index/inmobiliarias', 'API\InmobiliariaController@index');
 
 Route::group(['middleware' => ['guest:api']], function () {
-       Route::post('login', 'API\AuthController@login');
-       Route::post('register', 'API\AuthController@register');
-       Route::post('isAdminGuest', 'API\AuthController@isAdmin');
-       Route::get('changePass/{token}', 'API\PasswordResetController@changePass')->name('changePass');
+    Route::post('login', 'API\AuthController@login');
+    Route::post('register', 'API\AuthController@register');
+    Route::post('isAdminGuest', 'API\AuthController@isAdmin');
+    Route::get('changePass/{token}', 'API\PasswordResetController@changePass')->name('changePass');
 
-       // Reset password
-       Route::post('resetPassword/create', 'API\PasswordResetController@create');
-       Route::get('resetPassword/find/{token}', 'API\PasswordResetController@find');
-       Route::post('resetPassword/reset', 'API\PasswordResetController@reset')->name('resetPass');
+    // Reset password
+    Route::post('resetPassword/create', 'API\PasswordResetController@create');
+    Route::get('resetPassword/find/{token}', 'API\PasswordResetController@find');
+    Route::post('resetPassword/reset', 'API\PasswordResetController@reset')->name('resetPass');
+});
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('logout', 'API\AuthController@logout');
+    Route::post('isEntidadAuth', 'API\AuthController@isEntidad');
+    Route::post('isAdminAuth', 'API\AuthController@isAdmin');
+    Route::get('getUser', 'API\AuthController@getUser')->middleware('scope:admin,colaborador');
 
-   });
-   Route::group(['middleware' => 'auth:api'], function() {
-       Route::get('logout', 'API\AuthController@logout');
-       Route::post('isEntidadAuth', 'API\AuthController@isEntidad');
-       Route::post('isAdminAuth', 'API\AuthController@isAdmin');
-       Route::get('getUser', 'API\AuthController@getUser')->middleware('scope:admin,colaborador');
+    //Permisos
+    Route::post('permiso/index', 'API\PermisoController@index')->middleware('scope:admin,colaborador');
+    Route::post('permiso/list', 'API\PermisoController@list')->middleware('scope:admin,colaborador');
+    Route::post('permiso/store', 'API\PermisoController@store')->middleware('scope:admin,colaborador');
+    Route::post('permiso/show', 'API\PermisoController@show')->middleware('scope:admin,colaborador');
+    Route::post('permiso/update/{id}', 'API\PermisoController@update')->middleware('scope:admin,colaborador');
+    Route::post('permiso/destroy/{id}', 'API\PermisoController@destroy')->middleware('scope:admin,colaborador');
 
-        //Permisos
-        Route::post('permiso/index', 'API\PermisoController@index')->middleware('scope:admin,colaborador');
-        Route::post('permiso/list', 'API\PermisoController@list')->middleware('scope:admin,colaborador');
-        Route::post('permiso/store', 'API\PermisoController@store')->middleware('scope:admin,colaborador');
-        Route::post('permiso/show', 'API\PermisoController@show')->middleware('scope:admin,colaborador');
-        Route::post('permiso/update/{id}', 'API\PermisoController@update')->middleware('scope:admin,colaborador');
-        Route::post('permiso/destroy/{id}', 'API\PermisoController@destroy')->middleware('scope:admin,colaborador');
+    //Usuarios
+    Route::post('usuarios/index/{page?}', 'API\UserController@index')->middleware('scope:admin');
+    Route::post('usuarios/crear', 'API\UserController@store')->middleware('scope:admin');
+    Route::post('usuario/actualizar/{id}', 'API\UserController@update')->middleware('scope:admin');
+    Route::post('usuarios/cambiar-password', 'API\UserController@cambiarPassword')->middleware('scope:admin,colaborador');
+    Route::post('usuarios/eliminar/{id}', 'API\UserController@eliminarUsuario')->middleware('scope:admin');
+    Route::post('usuarios/miPerfil', 'API\UserController@miPerfil')->middleware('scope:admin,colaborador');
 
-        //Usuarios
-        Route::post('usuarios/index/{page?}','API\UserController@index')->middleware('scope:admin');
-        Route::post('usuarios/crear','API\UserController@store')->middleware('scope:admin');
-        Route::post('usuario/actualizar/{id}','API\UserController@update')->middleware('scope:admin');
-        Route::post('usuarios/cambiar-password','API\UserController@cambiarPassword')->middleware('scope:admin,colaborador');
-        Route::post('usuarios/eliminar/{id}','API\UserController@eliminarUsuario')->middleware('scope:admin');
-        Route::post('usuarios/miPerfil','API\UserController@miPerfil')->middleware('scope:admin,colaborador');
-
-        // Inmobiliaria
-        Route::post('inmobiliaria/store','API\InmobiliariaController@store')->middleware('scope:admin,colaborador');
-        Route::post('comprobar/inmobiliaria','API\InmobiliariaController@comprobar')->middleware('scope:admin,colaborador');
-        Route::post('actualizar/inmobiliaria','API\InmobiliariaController@actualizar')->middleware('scope:admin,colaborador');
-        Route::post('inmobiliaria/eliminar/{id}','API\InmobiliariaController@eliminarInmobiliaria')->middleware('scope:admin');
-        Route::post('editar/inmobiliaria/{id}','API\InmobiliariaController@editar')->middleware('scope:admin');
-
-
-        // Estado
-        Route::post('estado/store','API\EstadoController@store')->middleware('scope:admin');
-        Route::post('estados/eliminar/{id}','API\EstadoController@eliminarEstado')->middleware('scope:admin');
+    // Inmobiliaria
+    Route::post('inmobiliaria/store', 'API\InmobiliariaController@store')->middleware('scope:admin,colaborador');
+    Route::post('comprobar/inmobiliaria', 'API\InmobiliariaController@comprobar')->middleware('scope:admin,colaborador');
+    Route::post('actualizar/inmobiliaria', 'API\InmobiliariaController@actualizar')->middleware('scope:admin,colaborador');
+    Route::post('inmobiliaria/eliminar/{id}', 'API\InmobiliariaController@eliminarInmobiliaria')->middleware('scope:admin');
+    Route::post('editar/inmobiliaria/{id}', 'API\InmobiliariaController@editar')->middleware('scope:admin');
 
 
-        // Tipo
-        Route::post('tipo/store','API\TiposController@store')->middleware('scope:admin');
-        Route::post('tipo/eliminar/{id}','API\TipoController@eliminarTipo')->middleware('scope:admin');
-
-        // Viviendas
-        Route::post('viviendas/mis-viviendas/{page?}', 'API\ViviendaController@misViviendas')->middleware('scope:admin,colaborador');
-        Route::post('vivienda/store','API\ViviendaController@store')->middleware('scope:admin,colaborador');
-
-        // Estados
-        // ->middleware('scope:admim,colaborador');
+    // Estado
+    Route::post('estado/store', 'API\EstadoController@store')->middleware('scope:admin');
+    Route::post('estados/eliminar/{id}', 'API\EstadoController@eliminarEstado')->middleware('scope:admin');
 
 
-        // Tipos
-        // Route::post('tipos/index','API\TipoController@index')->middleware('scope:admin,colaborador');
+    // Tipo
+    Route::post('tipo/store', 'API\TiposController@store')->middleware('scope:admin');
+    Route::post('tipo/eliminar/{id}', 'API\TipoController@eliminarTipo')->middleware('scope:admin');
 
-        //Dashboard
-        Route::post('dashboard/usuarios','API\UserController@usuarios')->middleware('scope:admin');
+    // Viviendas
+    Route::post('viviendas/mis-viviendas/{page?}', 'API\ViviendaController@misViviendas')->middleware('scope:admin,colaborador');
+    Route::post('vivienda/store', 'API\ViviendaController@store')->middleware('scope:admin,colaborador');
+    Route::post('vivienda/actualizar/{id}', 'API\ViviendaController@update')->middleware('scope:admin,colaborador');
+    Route::post('vivienda/eliminar/{id}', 'API\ViviendaController@eliminarVivienda')->middleware('scope:admin,colaborador');
+
+    // Estados
+    // ->middleware('scope:admim,colaborador');
 
 
-    });
+    // Tipos
+    // Route::post('tipos/index','API\TipoController@index')->middleware('scope:admin,colaborador');
 
+    //Dashboard
+    Route::post('dashboard/usuarios', 'API\UserController@usuarios')->middleware('scope:admin');
+});
